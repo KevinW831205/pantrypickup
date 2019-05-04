46c2f9ce758d663bf1ac0de9b908d01f89a32921
iBrokeTheInternet <brandonotero14@gmail.com>
2019-05-04 01:49:25 -0400

    apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,62 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+  // Get all donations
+  app.get("/api/donations/", function(req, res) {
+    db.Food.findAll({}).then(function(dbFood) {
+      res.json(dbFood);
     });
   });
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-    });
+  // Get route for retrieving a single donator
+  app.get("/api/donations/:id", function(req, res) {
+    db.Food.findOne({
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(function(dbFood) {
+        res.json(dbFood);
+      });
   });
 
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
-    });
+  // POST route for saving a new donator
+  app.post("/api/donations", function(req, res) {
+    console.log(req.body);
+    db.Food.create({
+      donator: req.body.donator,
+      food: req.body.food,
+      size: req.body.size,
+      expiration: req.body.expiration,
+      donated: req.body.donated
+    })
+      .then(function(dbFood) {
+        res.json(dbFood);
+      });
+  });
+
+  // DELETE route for deleting donators
+  app.delete("/api/donations/:id", function(req, res) {
+    db.Food.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(function(dbFood) {
+        res.json(dbFood);
+      });
+  });
+
+  // PUT route for updating posts
+  app.put("/api/donations", function(req, res) {
+    db.Post.update(req.body,
+      {
+        where: {
+          id: req.body.id
+        }
+      })
+      .then(function(dbFood) {
+        res.json(dbFood);
+      });
   });
 };
