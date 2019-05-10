2f20828f76dcae51b684337e0b84ad641021f1a5
Shane Maziekien <shane.maziekien9@gmail.com>
2019-05-09 20:06:12 -0400

    chart.js fixes

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,6 @@
 M.AutoInit();
 
-$(document).ready(function() {
+$(document).ready(function () {
   $(".parallax").parallax();
 });
 
@@ -12,7 +12,7 @@ var $exampleList = $("#example-list");
 
 // The API object contains methods for each kind of request we'll make
 var API = {
-  saveExample: function(example) {
+  saveExample: function (example) {
     return $.ajax({
       headers: {
         "Content-Type": "application/json"
@@ -22,13 +22,13 @@ var API = {
       data: JSON.stringify(example)
     });
   },
-  getExamples: function() {
+  getExamples: function () {
     return $.ajax({
       url: "api/examples",
       type: "GET"
     });
   },
-  deleteExample: function(id) {
+  deleteExample: function (id) {
     return $.ajax({
       url: "api/examples/" + id,
       type: "DELETE"
@@ -37,9 +37,9 @@ var API = {
 };
 
 // refreshExamples gets new examples from the db and repopulates the list
-var refreshExamples = function() {
-  API.getExamples().then(function(data) {
-    var $examples = data.map(function(example) {
+var refreshExamples = function () {
+  API.getExamples().then(function (data) {
+    var $examples = data.map(function (example) {
       var $a = $("<a>")
         .text(example.text)
         .attr("href", "/example/" + example.id);
@@ -67,7 +67,7 @@ var refreshExamples = function() {
 
 // handleFormSubmit is called whenever we submit a new example
 // Save the new example to the db and refresh the list
-var handleFormSubmit = function(event) {
+var handleFormSubmit = function (event) {
   event.preventDefault();
 
   var example = {
@@ -80,7 +80,7 @@ var handleFormSubmit = function(event) {
     return;
   }
 
-  API.saveExample(example).then(function() {
+  API.saveExample(example).then(function () {
     refreshExamples();
   });
 
@@ -90,12 +90,12 @@ var handleFormSubmit = function(event) {
 
 // handleDeleteBtnClick is called when an example's delete button is clicked
 // Remove the example from the db and refresh the list
-var handleDeleteBtnClick = function() {
+var handleDeleteBtnClick = function () {
   var idToDelete = $(this)
     .parent()
     .attr("data-id");
 
-  API.deleteExample(idToDelete).then(function() {
+  API.deleteExample(idToDelete).then(function () {
     refreshExamples();
   });
 };
@@ -103,3 +103,43 @@ var handleDeleteBtnClick = function() {
 // Add event listeners to the submit and delete buttons
 $submitBtn.on("click", handleFormSubmit);
 $exampleList.on("click", ".delete", handleDeleteBtnClick);
+
+// Creating chart.js for food waste pie chart
+new Chart(document.getElementById("myChart"), {
+  type: "pie",
+  data: {
+    labels: [
+      "Residential",
+      "Full Service Restaurants",
+      "Quick Service Restaurants",
+      "Grocery Stores",
+      "Institutional",
+      "Industrial"
+    ],
+    datasets: [
+      {
+        label: "Annual U.S. Food Waste Disposal by Sector (tons)",
+        backgroundColor: [
+          "#FB2E02",
+          "#FBBB02",
+          "#77FB02",
+          "#02FBE1",
+          "#0291FB",
+          "#1102FB"
+        ],
+        data: [17369754, 7501380, 5100724, 4108821, 4000000, 659889]
+      }
+    ]
+  },
+  options: {
+    title: {
+      display: true,
+      fontsize: 14,
+      color: "#000000",
+      text: "Annual U.S. Food Waste Disposal by Sector (tons)"
+    },
+    legend: {
+      position: "left"
+    }
+  }
+});
